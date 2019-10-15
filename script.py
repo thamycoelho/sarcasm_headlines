9d62026c0d6f1d369b3c008f27f5b65d0f097ad2
Sitio <vinicius.c.e@hotmail.com>
2019-10-15 09:51:17 -0300

    Lemmatization, Stemming and pos tagging

--- script.py
+++ script.py
@@ -1,11 +1,15 @@
 from nltk import download
+from nltk import pos_tag
 from nltk import word_tokenize
 from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer
+from nltk.stem import WordNetLemmatizer # Lemmatizer of coice
+from nltk.stem import SnowballStemmer # Stemmer of choice
 
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import CountVectorizer
 
+import matplotlib.pyplot as plt
+from wordcloud import WordCloud
 from textblob import TextBlob
 from os.path import join
 import pandas as pd
@@ -15,44 +19,49 @@ import re
 download('punkt')
 download('stopwords')
 download('wordnet')
+download('averaged_perceptron_tagger')
+
+def big_line(text, tokenized=True, line=""):
+    for sentence in text : 
+        if tokenized : 
+            for word in sentence : 
+                line += " "+word 
+        else:
+            line += " "+sentence  
+    
+    return [line]
+
+def clean_up(text, pattern="[.,:%-?()&$'\"!\“\”¯°–―—_\/|#\[\]…@ツ¡\d]"):
+    # Cleanning up the data 
+    clean_up = lambda txt : re.sub(pattern, '', txt)
+    text = text.apply(clean_up)
 
 # Reading dataset
 folder = 'Dataset'
 dataset = pd.read_json(join(folder, 'Sarcasm_Headlines_Dataset_v2.json'), lines=True)
 
-# Perfectly balanced, as all things should be...
-sarcastic = dataset[dataset['is_sarcastic']==1]
-legit = dataset[dataset['is_sarcastic']==0]
-print(f'Sarcastic: {len(sarcastic)*100/(len(sarcastic)+len(legit))}%')
-print(f'Legitimate: {len(legit)*100/(len(sarcastic)+len(legit))}%')
+# Tokenize headlines (nltk tokenizer is more robust with punctuation)
+dataset.headline = dataset['headline'].apply(word_tokenize)
 
-# Cleanning up the data 
-pattern = "[.,:%-?()&$'\"!\“\”¯°–―—_\/|#\[\]…@ツ¡\d]" 
-clean_up = lambda txt : re.sub(pattern, '', txt)
-dataset.headline = dataset.headline.apply(clean_up)
+# Stemming words (good reduction of words)
+stemmer = SnowballStemmer('english')
+stemmezation = lambda words : [stemmer.stem(w) for w in words]
+dataset.headline = dataset.headline.apply(stemmezation)
 
-# Tokenize headlines
-token_head = dataset['headline'].apply(word_tokenize)
-print(token_head)
+# # Lemmatizing words (preserves more meaning)
+# wnlt = WordNetLemmatizer()
+# lemmatization = lambda phrase : [wnlt.lemmatize(w) for w in phrase]
+# dataset.headline = dataset.headline.apply(lemmatization)
 
-# Lemmatize all words
-lemmatizer = WordNetLemmatizer()
-lemmatization = lambda word : lemmatizer.lemmatize(word)
-dataset.headline = dataset.headline.apply(lemmatization)
+# Bagging words (gives a matrix with the count of each word)
+counter = CountVectorizer(stop_words='english')
+matrix = counter.fit_transform(big_line(dataset.headline))
+words = counter.get_feature_names()
+counts = matrix.toarray()
+bag = pd.DataFrame(counts, columns=words)
 
-# Subjectivity in sarcasm (not very usefull)
-sarcastic = dataset[dataset['is_sarcastic']==1]
-legit = dataset[dataset['is_sarcastic']==0]
-subjectivity = lambda x: TextBlob(x).sentiment.subjectivity
-sum(sarcastic.headline.apply(subjectivity))/len(sarcastic)
-sum(legit.headline.apply(subjectivity))/len(legit)
-
-# Create bag of words
-words_bags = CountVectorizer(stop_words='english')
-words_matrix = words_bags.fit_transform(dataset.headline)
-words = words_bags.get_feature_names()
-content = words_matrix.toarray()
-words_bags = pd.DataFrame(content, columns=words)
+# POS tagging (somewhat limited by the flexibility of words)
+tagged_data = pos_tag(bag.head())
 
 # Removing stopwords: common words that are less useful for detection (example:"the")
 stop = set(stopwords.words('english'))
@@ -66,3 +75,36 @@ X, X_test, Y, Y_test = train_test_split(dataset['headline'], dataset['is_sarcast
 print(X)
 print(Y)
     
+
+
+
+
+
+##### SOME TESTED STUFF WHICH WAS DISCARDED #####
+
+#### CHECKING DATASET DISTRIBUTION ####
+# # Perfectly balanced, as all things should be...
+# sarcastic = dataset[dataset['is_sarcastic']==1]
+# legit = dataset[dataset['is_sarcastic']==0]
+# print(f'Sarcastic: {len(sarcastic)*100/(len(sarcastic)+len(legit))}%')
+# print(f'Legitimate: {len(legit)*100/(len(sarcastic)+len(legit))}%')
+
+#### TESTING SUBJECTIVITY ####
+# # Subjectivity in sarcasm (not very usefull)
+# sarcastic = dataset[dataset['is_sarcastic']==1]
+# legit = dataset[dataset['is_sarcastic']==0]
+# subjectivity = lambda x: TextBlob(x).sentiment.subjectivity
+# sum(sarcastic.headline.apply(subjectivity))/len(sarcastic)
+# sum(legit.headline.apply(subjectivity))/len(legit)
+
+#### WORD CLOUDS ####
+# # Sarcastic word clouds
+# wordcloud = WordCloud(max_words=50, background_color="white").generate(big_line)
+# plt.imshow(wordcloud, interpolation='bilinear')
+# plt.axis("off")
+# plt.show()
+# # Legitimate word cloud
+# wordcloud = WordCloud(max_words=50, background_color="white").generate(big_line)
+# plt.imshow(wordcloud, interpolation='bilinear')
+# plt.axis("off")
+# plt.show()
